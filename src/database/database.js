9fecc9b212073c2016c7fcc3189e7813093fba5d
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-08-07 18:50:55 +0530

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Database = async()=>{
     try {
-        const connects = await mongoose.connect(`${process.env.MONGO_URI}/nextproject`);
+        const connects = await mongoose.connect(`${process.env.MONGO_URI}/Foodify`);
         console.log("Database connected ",connects.connection.host);
     } catch (error) {
         console.log("Database connection failed",error);
