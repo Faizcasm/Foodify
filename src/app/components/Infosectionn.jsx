ccf8e62713a5e52519a2800c32680b965bf94ea7
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-08-07 04:03:18 -0700

    Update Infosectionn.jsx

--- src/app/components/Infosectionn.jsx
+++ src/app/components/Infosectionn.jsx
@@ -1,58 +1,8 @@
-// "use client"
-// import React from 'react';
-// import styled from 'styled-components';
-// import { motion } from 'framer-motion';
-
-// const InfoSectionWrapper = styled.section`
-//   padding: 4rem 2rem;
-//   background-color: #ffffff;
-//   color:#1a1a1a;
-//   text-align: center;
-//   border-radius:20px;
-//   margin-top:20px;
-//   margin-bottom:20px;
-// `;
-
-// const InfoTitle = styled(motion.h2)`
-//   font-size: 1.5rem;
-//   margin-bottom: 1rem;
-// `;
-
-// const InfoText = styled(motion.p)`
-//   font-size: 1.2rem;
-//   max-width: 800px;
-//   margin: 0 auto;
-// `;
-
-// const InfoSection = () => {
-//   return (
-//     <InfoSectionWrapper>
-//       <InfoTitle
-//         initial={{ opacity: 0, y: 50 }}
-//         animate={{ opacity: 1, y: 0 }}
-//         transition={{ duration: 1 }}
-//       >
-//         Why choose us !
-//       </InfoTitle>
-//       <InfoText
-//         initial={{ opacity: 0, y: 50 }}
-//         animate={{ opacity: 1, y: 0 }}
-//         transition={{ duration: 1, delay: 0.5 }}
-//       >
-//         We provide the best food delivery service with a wide variety of
-//         delicious meals from top-rated restaurants. Experience fast, reliable,
-//         and affordable food delivery at your convenience.
-//       </InfoText>
-//     </InfoSectionWrapper>
-//   );
-// };
-
-// export default InfoSection;
 
 "use client";
 import React from 'react';
 import { motion } from 'framer-motion';
-import './Info.css'; // Import the CSS file
+import '@/app/components/Info.css'; 
 
 const InfoSection = () => {
   return (
