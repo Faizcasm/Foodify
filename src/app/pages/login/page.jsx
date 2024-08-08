19c58d534ad67cbefde12eadde789883a810e827
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-08-08 01:59:51 -0700

    Update page.jsx

--- src/app/pages/login/page.jsx
+++ src/app/pages/login/page.jsx
@@ -35,10 +35,12 @@ const handleSubmit = async (e) => {
       setUser(res.data.user)
       setIsLoggedIn(true)
     }
+    else{
     setUser(res.data.user); // Adjust based on your API response structure
     setIsLoggedIn(true);
     router.push('/');
     location.reload()
+    }
   } catch (err) {
     console.error('Login Error:', err);
     toast.error(err.response?.data?.message || 'Login failed');
